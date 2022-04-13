59af1fa97909e91d065ed8f41a2f71cfe9ad00ab
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-04-13 18:59:34 -0400

    update

--- script.js
+++ script.js
@@ -22,8 +22,13 @@ show.addEventListener('click', () => getInfo());
 
 async function getInfo() {
   const zipcode = input.value;
-  const res = await fetch (`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${access}`)
+  const res = await fetch (`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${access}&units=imperial`);
 
+  if(res.status === 404) {
+    alert("Zipcode is invalid");
+    input.value="";
+  }
+  else {
   const data = await res.json();
   console.log(data);
 
@@ -38,4 +43,5 @@ async function getInfo() {
   minimum.innerText = data.main.temp_min;
   maximum.innerText = data.main.temp_max;
   humidity.innerText = data.main.humidity;
+}
 }
