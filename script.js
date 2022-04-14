0c4d63fdc934e3b0aee6b66937fc0a1b89a443b0
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-04-13 22:34:20 -0400

    small changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var input = document.getElementById("zipcode");
-const show = document.getElementById("show-weather");
+const button = document.getElementById("show-weather");
 
 const city = document.getElementById("city-name");
 const latitude = document.getElementById("lat");
@@ -16,32 +16,34 @@ const minimum = document.getElementById("temp-min");
 const maximum = document.getElementById("temp-max");
 const humidity = document.getElementById("humidity");
 
-const access = "a976a3a413808e9bce7acd3ce8f361bc"
+const access = "a976a3a413808e9bce7acd3ce8f361bc";
 
-show.addEventListener('click', () => getInfo());
+button.addEventListener('click', () => getInfo());
 
 async function getInfo() {
   const zipcode = input.value;
-  const res = await fetch (`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${access}&units=imperial`);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${access}&units=imperial`);
 
-  if(res.status === 404) {
-    alert("Zipcode is invalid");
-    input.value="";
+  if (res.status === 404) {
+    alert("Zip code is not valid. Please try again.");
+    input.value = "";
+  } else {
+    const data = await res.json();
+    console.log(data);
+
+    city.innerText = data.name;
+    latitude.innerText = data.coord.lat;
+    longitude.innerText = data.coord.lon;
+
+    main.innerText = data.weather[0].main;
+    description.innerText = data.weather[0].description;
+
+    speed.innerText = data.wind.speed;
+    degree.innerText = data.wind.deg;
+
+    current.innerText = data.main.temp;
+    minimum.innerText = data.main.temp_min;
+    maximum.innerText = data.main.temp_max;
+    humidity.innerText = data.main.humidity;
   }
-  else {
-  const data = await res.json();
-  console.log(data);
-
-  city.innerText = data.name;
-  latitude.innerText = data.coord.lat;
-  longitude.innerText = data.coord.lon;
-  main.innerText = data.weather[0].main;
-  description.innerText = data.weather[0].description;
-  speed.innerText = data.wind.speed;
-  degree.innerText = data.wind.deg;
-  current.innerText = data.main.temp;
-  minimum.innerText = data.main.temp_min;
-  maximum.innerText = data.main.temp_max;
-  humidity.innerText = data.main.humidity;
-}
 }
